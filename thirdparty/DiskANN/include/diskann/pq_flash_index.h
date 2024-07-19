eeea456282a56f06118eb850c349566a83d9f9f1
zihengsjtu@gmail.com <zilliz@zillizdeMacBook-Pro-7.local>
2024-07-19 12:09:13 +0800

    fix

--- thirdparty/DiskANN/include/diskann/pq_flash_index.h
+++ thirdparty/DiskANN/include/diskann/pq_flash_index.h
@@ -236,7 +236,7 @@ struct IteratorWorkspace {
 
     
     std::unique_ptr<IteratorWorkspace> getIteratorWorkspace(
-      const T *query_data, const _u64 ef, _s64 *indices, const _u64 beam_width,
+      T *query_data, const _u64 ef, _s64 *indices, const _u64 beam_width,
       const bool use_reorder_data, const float filter_ratio_in,
       const bool for_tun, const knowhere::BitsetView &bitset);
 
